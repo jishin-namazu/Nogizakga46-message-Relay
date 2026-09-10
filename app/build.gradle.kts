59fcf246304c1361b89d840dd039e6feb267d1df
fsguoguo <fsguoguo@outlook.com>
2026-09-10 12:23:39 +0800

    Add Android unit test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,4 +81,6 @@ dependencies {
     implementation("com.google.firebase:firebase-messaging")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.json:json:20240303")
 }
